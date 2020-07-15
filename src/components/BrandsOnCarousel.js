4e249793ffc12b6165bea1cd23748f4c1d6c66d4
Gabino Santana <gabino_18_@hotmail.com>
2020-07-15 01:08:13 -0300

    se mejora home, se cambia de lugar logos

--- src/components/BrandsOnCarousel.js
+++ src/components/BrandsOnCarousel.js
@@ -13,22 +13,9 @@ function BrandsOnCarousel() {
         style={{
           width: '270px',
           height: '176px',
-          paddingRight: '30px',
-          paddingLeft: '30px',
           borderRadius: '25% 0px 0px 25%',
         }}
         className="roll-in-left"
-      />
-      <img
-        src={img11}
-        alt=""
-        style={{
-          width: '270px',
-          height: '176px',
-          paddingRight: '30px',
-          paddingLeft: '30px',
-        }}
-        className="bounce-in-top"
       />
       <img
         src={img12}
@@ -36,9 +23,19 @@ function BrandsOnCarousel() {
         style={{
           width: '570px',
           height: '176px',
-          paddingRight: '30px',
-          paddingLeft: '30px',
-          borderRadius: '0px 14% 14% 0px',
+
+          //borderRadius: '0px 14% 14% 0px',
+        }}
+        className="bounce-in-top"
+      />
+      <img
+        src={img11}
+        alt=""
+        style={{
+          width: '270px',
+          height: '176px',
+          borderRadius: '0px 18% 18% 0px',
+          backgroundColor: 'white',
         }}
         className="roll-in-right"
       />
